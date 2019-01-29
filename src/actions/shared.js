9e7605e917b5bee8a55312730bb0a6ed85e75ed2
Taylor <taylor.nodell@gmail.com>
2019-01-29 13:37:55 -0500

    adds loading bar

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,16 +2,21 @@ import { getInitialData } from '../utils/api'
 import { receiveUsers } from '../actions/users'
 import { receiveTweets } from '../actions/tweets'
 import { setAuthedUser } from '../actions/authedUser'
+
+import {showLoading, hideLoading } from 'react-redux-loading'
+
 // mock a authedUser
 const AUTHED_ID = 'tylermcginnis'
 
 export const handleInitialData = () => {
 	return (dispatch) => {
+		dispatch(showLoading())
 		return getInitialData()
 			.then(({users, tweets}) =>{
 				dispatch(receiveUsers(users))
 				dispatch(receiveTweets(tweets))
 				dispatch(setAuthedUser(AUTHED_ID))
+				dispatch(hideLoading())
 			})
 	}
 }
